6786178ce5784dd623c46f279d792df34aaa4762
Vlad Zaharia <vlad@zaharia.io>
2020-08-04 13:11:25 -0700

    Bring back edge network

--- config/edge.tf
+++ config/edge.tf
@@ -37,69 +37,69 @@ variable "edgeNodes" {
   }))
 
   default = {
-    # "cac" = {
-    #   location   = "cac"
-    #   azLocation = "canadacentral"
-    #   name       = "Canada Central"
-    #   region     = "na"
-    #   ring       = 1
-    # },
-    # "cus" = {
-    #   location   = "cus"
-    #   azLocation = "centralus"
-    #   name       = "Central US"
-    #   region     = "na"
-    #   ring       = 1
-    # },
-    # "eus" = {
-    #   location   = "eus"
-    #   azLocation = "eastus"
-    #   name       = "East US"
-    #   region     = "na"
-    #   ring       = 1
-    # },
-    # "scus" = {
-    #   location   = "scus"
-    #   azLocation = "southcentralus"
-    #   name       = "South Central US"
-    #   region     = "na"
-    #   ring       = 1
-    # },
-    # "wus" = {
-    #   location   = "wus"
-    #   azLocation = "westus"
-    #   name       = "West US"
-    #   region     = "na"
-    #   ring       = 1
-    # },
-    # "neu" = {
-    #   location   = "neu"
-    #   azLocation = "northeurope"
-    #   name       = "North Europe"
-    #   region     = "eu"
-    #   ring       = 1
-    # },
-    # "weu" = {
-    #   location   = "weu"
-    #   azLocation = "westeurope"
-    #   name       = "West Europe"
-    #   region     = "eu"
-    #   ring       = 1
-    # },
-    # "jpe" = {
-    #   location   = "jpe"
-    #   azLocation = "japaneast"
-    #   name       = "Japan East"
-    #   region     = "asia"
-    #   ring       = 1
-    # },
-    # "seasia" = {
-    #   location   = "seasia"
-    #   azLocation = "southeastasia"
-    #   name       = "Southeast Asia"
-    #   region     = "asia"
-    #   ring       = 0
-    # }
+    "cac" = {
+      location   = "cac"
+      azLocation = "canadacentral"
+      name       = "Canada Central"
+      region     = "na"
+      ring       = 1
+    },
+    "cus" = {
+      location   = "cus"
+      azLocation = "centralus"
+      name       = "Central US"
+      region     = "na"
+      ring       = 1
+    },
+    "eus" = {
+      location   = "eus"
+      azLocation = "eastus"
+      name       = "East US"
+      region     = "na"
+      ring       = 1
+    },
+    "scus" = {
+      location   = "scus"
+      azLocation = "southcentralus"
+      name       = "South Central US"
+      region     = "na"
+      ring       = 1
+    },
+    "wus" = {
+      location   = "wus"
+      azLocation = "westus"
+      name       = "West US"
+      region     = "na"
+      ring       = 1
+    },
+    "neu" = {
+      location   = "neu"
+      azLocation = "northeurope"
+      name       = "North Europe"
+      region     = "eu"
+      ring       = 1
+    },
+    "weu" = {
+      location   = "weu"
+      azLocation = "westeurope"
+      name       = "West Europe"
+      region     = "eu"
+      ring       = 1
+    },
+    "jpe" = {
+      location   = "jpe"
+      azLocation = "japaneast"
+      name       = "Japan East"
+      region     = "asia"
+      ring       = 1
+    },
+    "seasia" = {
+      location   = "seasia"
+      azLocation = "southeastasia"
+      name       = "Southeast Asia"
+      region     = "asia"
+      ring       = 0
+    }
   }
 }
 
